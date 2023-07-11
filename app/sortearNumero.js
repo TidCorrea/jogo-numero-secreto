4eabf6f67a14e85fc2dbfc9637968216e3afe08b
Tiago Correa <90696534+TidCorrea@users.noreply.github.com>
2023-07-11 08:26:16 -0300

    Update sortearNumero.js

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -6,7 +6,7 @@ function gerarNumeroAleatorio() {
     return parseInt(Math.random() * maiorValor + 1);
 }
 
-console.log(numeroSecreto)
+//console.log(numeroSecreto)
 
 const elementoMenorValor = document.getElementById('menor-valor')
 elementoMenorValor.innerHTML = menorValor
